94b666ee9201d6f7344de5ee73954cb06c7ea2cc
Francisvan Oliveira <francisvan.oliveira@francisvanoliveira.com.br>
2022-06-05 23:26:22 -0400

    :package: Cookies

--- js/script.js
+++ js/script.js
@@ -78,3 +78,21 @@ $(document).ready(function(){
     });
 
 });
+
+//INICIO SCRIPT PARA COOKIES
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
+
+//FIM SCRIPT PARA COOKIES
